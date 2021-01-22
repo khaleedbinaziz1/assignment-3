1293c5abf34c0b84d9c3eaa52469ab5fc08e9c85
Khaled Bin Aziz <khaleedbinaziz@gmail.com>
2021-01-22 22:30:10 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,18 @@
+
+//problem-1
+function kilometerTOMeter(kilometer){
+    const meter = kilometer * 1000;
+    return meter;
+}
+const convert =  [2];
+
+const result = kilometerTOMeter(convert[0]);
+console.log(result)
+
+//problem-4
+function megaFriend(str) {
+    return str.length;
+  }
+  findLongestWord("rahim karim abdul fahim");
+  var strSplit = “The quick brown fox jumped over the lazy dog”.split(‘ ‘);
+  
